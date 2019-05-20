a7e4373067c684c7604b4366fe686698d3c87b11
Unknown <sam.gawith@gmail.com>
2019-05-20 20:50:38 +0100

    JSON definitions

--- json-schema-simplifier-maven-plugin/src/main/java/com/kestreldigital/jsonschema/simplifier/plugin/JsonSimplifierMojo.java
+++ json-schema-simplifier-maven-plugin/src/main/java/com/kestreldigital/jsonschema/simplifier/plugin/JsonSimplifierMojo.java
@@ -1,6 +1,6 @@
 package com.kestreldigital.jsonschema.simplifier.plugin;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kestreldigital.jsonschema.simplifier.plugin.draft07.Draft07SchemaSimplifier;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -11,7 +11,8 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mojo(name="simplify")
 public class JsonSimplifierMojo extends AbstractMojo {
@@ -19,36 +20,30 @@ public class JsonSimplifierMojo extends AbstractMojo {
     @Parameter(property="sourceDirectory")
     private String sourceDirectory;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
         if (sourceDirectory == null || "".equals(sourceDirectory)) {
             throw new MojoExecutionException("No source path specified - configuration parameter sourceDirectory is required.");
         }
-
         sourceDirectory = FilenameUtils.normalize(sourceDirectory);
-
         getLog().info("Searching for JSON schema files on path " + sourceDirectory);
-
-        File dir = new File(sourceDirectory);
-        File[] directoryListing = dir.listFiles();
-        if (directoryListing != null) {
-            for (File child : directoryListing) {
-                if (child.getPath().endsWith(".json")) {
-                    getLog().info("Found json schema file: " + child.getName());
-                    try {
-                        final String schemaBody = new String(Files.readAllBytes(child.toPath()));
-
-                    } catch (IOException e) {
-                        throw new MojoExecutionException("Unable to read schema file " + child.getName());
-                    }
-                }
-            }
-        } else {
+        final File dir = new File(sourceDirectory);
+        final File[] directoryListing = dir.listFiles();
+        final List<File> schemaFiles = new ArrayList<>();
+        if (directoryListing == null) {
             throw new MojoExecutionException("Invalid source path - path " + sourceDirectory + " was not found.");
         }
-
+        for (File child : directoryListing) {
+            if (child.getPath().endsWith(".json")) {
+                getLog().debug("Found json schema file: " + child.getName());
+                schemaFiles.add(child);
+            }
+        }
+        SchemaSimplifier schemaSimplifier = new Draft07SchemaSimplifier(getLog());
+        try {
+            schemaSimplifier.process(schemaFiles, "");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Unable to parse JSON files.", e);
+        }
     }
 }
